6d7d488febf4e49a2ff41243e8e1f87ffeac7abd
Steven Savery <steven.savery@outlook.com>
2015-06-05 16:08:00 +0100

    LESS added to project

--- Client/Gruntfile.js
+++ Client/Gruntfile.js
@@ -1,6 +1,41 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+
+        paths: {
+            lessSrc : 'less/site.less',
+            lessDest: 'app/style.css'
+        },
+
+        less: {
+            options: {
+                paths: ['less']
+            },
+            development: {
+                options: {
+                    sourceMap: true,
+                    sourceMapFilename: 'style.css.map',
+                    outputSourceFiles: true
+                },
+                files: {
+                    '<%= paths.lessDest %>': '<%= paths.lessSrc %>'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                    ieCompat: true
+                },
+                files: {
+                    '<%= paths.lessDest %>': '<%= paths.lessSrc %>'
+                }
+            }
+        },
+
+        clean: {
+            styleMap: ['style.css.map']
+        },
+
         nodemon: {
             dev: {
                 script: 'run.js'
@@ -9,7 +44,11 @@ module.exports = function(grunt) {
 
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-nodemon');
 
-    grunt.registerTask('default', ['nodemon']);
+    grunt.registerTask('build', ['styles', 'nodemon']);
+    grunt.registerTask('styles', ['clean:styleMap', 'less:production']);
+    grunt.registerTask('default', ['build']);
 };
