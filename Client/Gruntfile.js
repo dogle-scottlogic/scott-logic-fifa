f6f960d53785e51ee6eb82427172d238e06c0002
Steven Savery <steven.savery@outlook.com>
2015-06-19 16:37:50 +0100

    Build Changes

--- Client/Gruntfile.js
+++ Client/Gruntfile.js
@@ -4,7 +4,12 @@ module.exports = function(grunt) {
 
         paths: {
             lessSrc : 'less/site.less',
-            lessDest: 'app/style.css'
+            lessDest: 'app/style.css',
+            jsSrc: [
+                'lib/jquery/dist/jquery.js',
+                'lib/bootstrap/dist//js/bootstrap.js',
+            ],
+            jsDest: 'app/script.js',
         },
 
         less: {
@@ -33,12 +38,34 @@ module.exports = function(grunt) {
         },
 
         clean: {
-            styleMap: ['style.css.map']
+            styleMap: ['style.css.map'],
+            scriptMap: ['script.js.map']
         },
 
         nodemon: {
             dev: {
                 script: 'run.js'
+            }
+        },
+
+        uglify: {
+            development: {
+                options: {
+                    sourceMap: true,
+                    sourceMapIncludeSources: true
+                },
+                files: {
+                    '<%= paths.jsDest %>': '<%= paths.jsSrc %>'
+                }
+            },
+            production: {
+                options: {
+                    mangle: true,
+                    compress: true
+                },
+                files: {
+                    '<%= paths.jsDest %>': '<%= paths.jsSrc %>'
+                }
             }
         }
 
@@ -46,9 +73,11 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-nodemon');
 
-    grunt.registerTask('build', ['styles', 'nodemon']);
+    grunt.registerTask('build', ['styles', 'scripts', 'nodemon']);
     grunt.registerTask('styles', ['clean:styleMap', 'less:production']);
+    grunt.registerTask('scripts', ['clean:scriptMap', 'uglify:production']);
     grunt.registerTask('default', ['build']);
 };
