eba39a3d35e32221c32dbf3e327f1935c3a1c1f1
fpozzobon <fpozzobon@scottlogic.co.uk>
2015-07-02 08:57:20 +0100

    Match model : Created a Scores table which replace the scores columns from the Matches table. Added a migration in consequence.

--- Server/FIFA.Server/Migrations/Configuration.cs
+++ Server/FIFA.Server/Migrations/Configuration.cs
@@ -1,6 +1,5 @@
 namespace FIFA.Server.Migrations
 {
-    using FIFA.Server.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -10,14 +9,23 @@ namespace FIFA.Server.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(FIFA.Server.Models.FIFAServerContext context)
         {
-            context.Players.AddOrUpdate(x => x.Id,
-                new Player() { Id = 2, Name = "Steven" }
-            );
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
         }
     }
 }
---
+++ Server/FIFA.Server/Models/Score.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FIFA.Server.Models
+{
+    public enum Location
+    {
+        Home = 1,
+        Away = 2
+    }
+
+    public class Score
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Match"), Column(Order = 0)]
+        public int MatchId { get; set; }
+
+        [ForeignKey("Player"), Column(Order = 0)]
+        public int PlayerId { get; set; }
+        
+        public int Goals { get; set; }
+
+        public virtual Match Match { get; set; }
+        public virtual Player Player { get; set; }
+
+        [Range(1, 2), Display(Name = "Location")]
+        public Location Location { get; set; }
+
+    }
+}
